bcedf6fb77addbe5c23bf6e2e8981b6dac30310f
Lieven <lieven@risolto.be>
2023-12-01 15:54:30 +0100

    added_util

---
+++ util.erl
@@ -0,0 +1,9 @@
+-module(util).
+-export([timed/1]).
+
+
+timed(F) ->
+    Start = erlang:system_time(microsecond),
+    Result = F(),
+    End = erlang:system_time(microsecond),
+    {{result, Result}, {microsecs, End-Start}}.
