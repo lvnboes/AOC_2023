15b355661a243374b24d588f73b20489e66cfaa2
lvnboes <lvn.boes@gmail.com>
2023-12-03 02:12:15 +0100

    expanded utils

--- util.erl
+++ util.erl
@@ -1,5 +1,5 @@
 -module(util).
--export([timed/1, c/0, c/1]).
+-export([timed/1, c/0, c/1, r/0, r/1, r/2, rt/0, rt/1, cr/0, cr/1, crt/0, crt/1]).
 
 %Timer
 
@@ -11,12 +11,6 @@ timed(F) ->
 
 %Compile all modules
 
-find_erl_files() ->
-    {ok, Files} = file:list_dir("."),
-    [File || File <- Files, check_erl(File)].
-
-check_erl(File_name) -> lists:sublist(lists:reverse(File_name), 4) == "lre.".
-
 compile_modules(Files) -> compile_modules(Files, []).
 compile_modules([], Compiled) -> {ok,lists:sort(Compiled)};
 compile_modules([FileName|FileNames], Compiled) -> 
@@ -25,11 +19,70 @@ compile_modules([FileName|FileNames], Compiled) ->
             compile_modules(FileNames, Compiled);
         true -> 
             compile:file(FileName),
-            CurrentCompiled = [list_to_atom(lists:sublist(FileName, length(FileName)-4))|Compiled],
+            CurrentCompiled = [list_to_atom(lists:sublist(FileName, length(FileName) - 4))|Compiled],
             compile_modules(FileNames, CurrentCompiled)
     end.
 
 c() -> 
     Files = find_erl_files(),
     compile_modules(Files).
-c(Day) -> compile:file("day"++integer_to_list(Day)++".erl").
+c(DayId) -> compile:file("day"++integer_to_list(DayId)++".erl").
+
+%Execute_all_days
+
+is_day(FileName) -> 
+    lists:sublist(string:lowercase(FileName), 1, 3) == "day" 
+    andalso 
+    lists:sublist(string:lowercase(FileName), length(FileName) - 3, length(FileName)) == ".erl".
+
+get_file_number(FileName) -> 
+    StrippedName = rm_ss(rm_ss(string:lowercase(FileName), ".erl"), "day"),
+    try list_to_integer(StrippedName) of Val -> Val
+    catch error:_ -> 99
+    end.
+
+day_precedes(A, B) -> get_file_number(A) >= get_file_number(B).
+
+run(Modules, Function) -> run(Modules, Function, []).
+run([], _Function, Results) -> maps:from_list(Results);
+run([Module|Modules], Function, Results) -> 
+    Exports = maps:from_list(Module:module_info(exports)),
+    Exists = maps:is_key(Function, Exports) and (maps:get(Function, Exports) == 0),
+    Result = if
+        Exists -> {Module, Module:Function()};
+        true -> {Module, #{function => Function, result => not_found}}
+    end,
+    run(Modules, Function, [Result|Results]).
+
+
+r() -> r(solve).
+r(DayId) when is_integer(DayId) -> r(DayId, solve);
+r(timed) -> r(solve_timed);
+r(Function) ->
+    Files = lists:sort(fun day_precedes/2, lists:filter(fun is_day/1, find_erl_files())),
+    Modules = [list_to_atom(lists:flatten(rm_ss(FileName, ".erl"))) || FileName <- Files],
+    run(Modules, Function).
+r(DayId, timed) -> r(DayId, solve_timed);
+r(DayId, Function) when is_integer(DayId) -> 
+    Module = list_to_atom("day"++integer_to_list(DayId)),
+    run([Module], Function).
+
+rt() -> r(timed).
+rt(DayId) -> r(DayId, timed).
+
+cr() -> {c(), r()}.
+cr(DayId) -> {c(DayId), r(DayId)}.
+
+crt() -> {c(), rt()}.
+crt(DayId) -> {c(DayId), r(DayId)}.
+
+
+%Common
+
+find_erl_files() ->
+    {ok, Files} = file:list_dir("."),
+    [File || File <- Files, check_erl(File)].
+
+check_erl(File_name) -> lists:sublist(lists:reverse(File_name), 4) == "lre.".
+
+rm_ss(String, Remove) -> lists:flatten(string:replace(String, Remove, "")).
