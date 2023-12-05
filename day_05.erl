419a8ef80b957dbb7f6fd85703b13b5dedb7b0fb
lvnboes <lvn.boes@gmail.com>
2023-12-05 19:34:54 +0100

    parsing cleanup

--- day_05.erl
+++ day_05.erl
@@ -5,12 +5,17 @@
 process_input(Path) ->
     {ok, File} = file:read_file(Path),
     [Seeds, ConversionMaps] = split(File, <<"\n\n">>),
-    {lists:droplast(lists:reverse(split(Seeds, <<" ">>, [global]))),[parse_conv_map(ConvMap) || ConvMap <- split(ConversionMaps, <<"\n\n">>, [global])]
-}.
+    {
+        lists:droplast(lists:reverse(split(Seeds, <<" ">>, [global]))),
+        [parse_conv_map(ConvMap) || ConvMap <- split(ConversionMaps, <<"\n\n">>, [global])]
+    }.
 
 parse_conv_map(ConvMap) -> 
     [Conv, Map] = split(ConvMap, <<":\n">>),
-    {split(lists:nth(1, split(Conv, <<" ">>)), <<"-to-">>), [[binary_to_integer(Val) || Val <- split(Line, <<" ">>, [global])] || Line <- split(Map, <<"\n">>, [global])]}.
+    {
+        split(lists:nth(1, split(Conv, <<" ">>)), <<"-to-">>), 
+        [[binary_to_integer(Val) || Val <- split(Line, <<" ">>, [global])] || Line <- split(Map, <<"\n">>, [global])]
+    }.
 
 solve() -> process_input("./data/test.aoc").
 solve_timed() -> util:timed(fun solve/0).
