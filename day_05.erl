ac24d95a656c331d13eb3b1d14bbea9dd36e1675
lvnboes <lvn.boes@gmail.com>
2023-12-05 20:40:27 +0100

    day 5 part 1

--- day_05.erl
+++ day_05.erl
@@ -6,7 +6,7 @@ process_input(Path) ->
     {ok, File} = file:read_file(Path),
     [Seeds, ConversionMaps] = split(File, <<"\n\n">>),
     {
-        lists:droplast(lists:reverse(split(Seeds, <<" ">>, [global]))),
+        [binary_to_integer(Seed) || Seed <- lists:droplast(lists:reverse(split(Seeds, <<" ">>, [global])))],
         [parse_conv_map(ConvMap) || ConvMap <- split(ConversionMaps, <<"\n\n">>, [global])]
     }.
 
@@ -17,5 +17,26 @@ parse_conv_map(ConvMap) ->
         [[binary_to_integer(Val) || Val <- split(Line, <<" ">>, [global])] || Line <- split(Map, <<"\n">>, [global])]
     }.
 
-solve() -> process_input("./data/test.aoc").
+find_destination(Seed, {_Header, []}) -> Seed;
+find_destination(Seed, {_Header, [[Dest, Source, Range] | _Conversions]}) 
+  when (Seed >= Source) andalso (Seed =< (Source + Range - 1)) ->
+    Dest + Seed - Source;
+find_destination(Seed, {Header, [_Conv | Conversions]}) -> 
+    find_destination(Seed, {Header, Conversions}).
+
+find_location(Seed, []) -> Seed;
+find_location(Seed, [ConvTable | ConversionTables]) -> 
+    find_location(find_destination(Seed, ConvTable), ConversionTables).
+
+find_locations({[], _Tables}, Locations) -> Locations;
+find_locations({[Seed | Seeds], Tables}, Locations) -> 
+    find_locations({Seeds, Tables}, [find_location(Seed, Tables) | Locations]).
+
+solve() -> 
+    Input = process_input("./data/day_05.aoc"),
+    #{
+        part1 =>lists:min(find_locations(Input, [])),
+        part2 => undefined
+    }.
+
 solve_timed() -> util:timed(fun solve/0).
