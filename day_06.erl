758dd93c858fb283444596acead7942fe8a08542
Lieven <lieven@risolto.be>
2023-12-06 10:58:29 +0100

    day 6 - both parts

---
+++ day_06.erl
@@ -0,0 +1,40 @@
+-module(day_06).
+-export([solve/0, solve_timed/0]).
+
+%Reading and basic parsing of input
+
+process_input(Path) ->
+    {ok, File} = file:read_file(Path),
+    binary:split(File, <<"\n">>).
+
+%Part 1
+
+parse_for_part_1(Input) -> 
+    [Times, Dists] = [[binary_to_integer(N) || N <- lists:sublist([Seg || Seg <- binary:split(Line, <<" ">>, [global]), Seg =/= <<"">>], 2, 9999)] || Line <- Input],
+    lists:zip(Times, Dists).
+
+ways_to_win_multiplied(TimesAndDists) -> 
+    multiply_all([calculate_ways_to_win(TimeAndDist) || TimeAndDist <- TimesAndDists]).
+
+multiply_all([]) -> 1;
+multiply_all([H|T]) -> H * multiply_all(T).
+
+%Part 2
+
+parse_for_part_2(Input) ->
+    list_to_tuple([binary_to_integer(binary:replace(lists:nth(2, binary:split(Line, <<" ">>)), <<" ">>, <<"">>, [global])) || Line <- Input]).
+
+%Common
+
+calculate_ways_to_win({TT, D}) -> 
+    Sqrt = math:sqrt(math:pow(TT, 2) - 4*D),
+    trunc(((-TT - Sqrt) / -2) - 0.0001) - round(((-TT + Sqrt) / -2) + 0.5) + 1.
+
+solve() ->
+    Input = process_input("./data/day_06.aoc"),
+    #{
+        part1 => ways_to_win_multiplied(parse_for_part_1(Input)),
+        part2 => calculate_ways_to_win(parse_for_part_2(Input))
+    }.
+
+solve_timed() -> util:timed(fun solve/0).
