138f70cee7f1c9bf43498c35e5521eea2fca09b8
Lieven <lieven@risolto.be>
2023-12-09 00:22:07 +0100

    day 8 - both parts

--- day_08.erl
+++ day_08.erl
@@ -1,6 +1,8 @@
 -module(day_08).
 -export([solve/0, solve_timed/0]).
 
+%Reading and parsing input
+
 process_imports(Path) ->
     {ok, File} = file:read_file(Path),
     [Instructions, Nodes] = binary:split(File, <<"\n\n">>),
@@ -17,33 +19,66 @@ process_imports(Path) ->
     {binary_to_list(Instructions), maps:from_list(ParsedNodes)}.
 
 parse_line([Node, LR]) -> 
-    {Node, list_to_tuple(binary:split(LR, <<", ">>))}.
+    {binary_to_list(Node), list_to_tuple([binary_to_list(PathOption) || PathOption <- binary:split(LR, <<", ">>)])}.
 
-execute_instructions(Node, Instructions, Map) -> 
-    execute_instructions(Node, Instructions, [], Map, 0).
-execute_instructions(<<"ZZZ">>, _Instructions, _Executed, _Map, Count) -> 
-    Count;
-execute_instructions(Node, [Instruction], Executed, Map, Count) 
-  when Instruction == $L -> 
-    {L, _R} = maps:get(Node, Map),
-    Instructions = lists:reverse([Instruction|Executed]),
-    execute_instructions(L, Instructions, [], Map, Count+1);
-execute_instructions(Node, [Instruction | T], Executed, Map, Count) 
-  when Instruction == $L -> 
-    {L, _R} = maps:get(Node, Map),
-    execute_instructions(L, T, [Instruction | Executed], Map, Count+1);
-execute_instructions(Node, [Instruction], Executed, Map, Count) 
-  when Instruction == $R -> 
-    {_L, R} = maps:get(Node, Map),
-    Instructions = lists:reverse([Instruction|Executed]),
-    execute_instructions(R, Instructions, [], Map, Count+1);
-execute_instructions(Node, [Instruction|T], Executed, Map, Count) 
-  when Instruction == $R -> 
-    {_L, R} = maps:get(Node, Map),
-    execute_instructions(R, T, [Instruction | Executed], Map, Count+1).
+%Part 2
+
+get_part_2_start_nodes(NodesMap) ->
+    lists:filter(
+        fun(N) -> lists:nth(3, N) == $A end, 
+        [Node || {Node, _Directions} <- maps:to_list(NodesMap)]
+    ).
+
+find_finishes(Starts, FinishCond, Instructions, NodesMap) -> 
+    find_finishes(Starts, FinishCond, Instructions, NodesMap, []).
+find_finishes([], _FinishCond, _Instructions, _NodesMap, Finishes) ->
+    Finishes;
+find_finishes([Start | Starts], FinishCond, Instructions, NodesMap, Finishes) ->
+    find_finishes(
+        Starts, FinishCond, Instructions, NodesMap, 
+        [execute_instructions(Start, FinishCond, Instructions, NodesMap) | Finishes]
+    ).
+
+calc_lcm([A]) -> A;
+calc_lcm([A, B | T]) -> calc_lcm([lcm(A, B) | T]).
+
+gcd(A,B) when A == 0; B == 0 -> 0;
+gcd(A,B) when A == B -> A;
+gcd(A,B) when A > B -> gcd(A-B, B);
+gcd(A,B) -> gcd(A, B-A).
+
+lcm(A,B) -> (A*B) div gcd(A, B).
+
+%Common
+
+execute_instructions(Node, FinishCond, Instructions, Map) -> 
+    execute_instructions(Node, FinishCond, Instructions, [], Map, 0).
+
+execute_instructions(Node, FinishCond, [Instruction|T], Executed, Map, Count) ->
+    case FinishCond(Node) of
+        true -> Count;
+        false ->
+            if 
+                length(T) == 0 -> 
+                    Instructions = lists:reverse([Instruction|Executed]),
+                    ExecInstr = [];
+                true -> 
+                    Instructions = T,
+                    ExecInstr = [Instruction | Executed]
+            end,
+            case Instruction of
+                $L -> {N, _R} = maps:get(Node, Map);
+                $R -> {_L, N} = maps:get(Node, Map)
+            end,
+        execute_instructions(N, FinishCond, Instructions, ExecInstr, Map, Count+1)
+    end.
 
 solve() -> 
     {Instructions, NodesMap} = process_imports("./data/day_08.aoc"),
-    execute_instructions(<<"AAA">>, Instructions, NodesMap).
+    Part1 = execute_instructions("AAA", fun(X) -> X == "ZZZ" end, Instructions, NodesMap),
+    StartNodes2 = get_part_2_start_nodes(NodesMap),
+    Finishes = find_finishes(StartNodes2, fun(X) -> lists:nth(3, X) == $Z end, Instructions, NodesMap),
+    Part2 = calc_lcm(Finishes),
+    #{part1 => Part1, part2 => Part2}.
 
 solve_timed() -> util:timed(fun solve/0).
